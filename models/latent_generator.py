922663441e7084171ba36aff3b1e61b03d569700
Jiachen Zhu <jiachen.zhu@nyu.edu>
2022-07-11 19:21:08 -0400

    add all

--- models/latent_generator.py
+++ models/latent_generator.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 class LatentGenerator(nn.Module):
     def __init__(self, representation_dim, hidden_dim, num_layers, latent_dim):
         super().__init__()
-        self.gru = nn.GRU(representation_dim, hidden_dim, num_layers, batch_first=True, bidirectional=True)
+        self.gru = nn.GRU(representation_dim, hidden_dim, num_layers, batch_first=True, bidirectional=False)
         self.fc = nn.Linear(hidden_dim, latent_dim)
 
     def forward(self, x):
