6b89c8660d80ab66f325ff7a50353230ac2ed36d
Jiachen Zhu <jiachen.zhu@nyu.edu>
2022-07-11 20:26:05 -0400

    add all

--- models/predictor.py
+++ models/predictor.py
@@ -9,18 +9,18 @@ class Predictor(nn.Module):
 
     def forward(self, initial_state, latent_variable, num_steps):
         # initial_state shape n l c
-        # latent_variable shape n l latent
+        # latent_variable shape n latent
         # predictor shape n l c c
 
         next_state_ground_true = initial_state[:, num_steps:]
 
         n, l, c = initial_state.shape
-        predictor = self.predictor_dictionary(latent_variable).view(n, l, c, c)
+        predictor = self.predictor_dictionary(latent_variable).view(n, c, c)
 
         # predictions = []
         next_state = initial_state
         for i in range(num_steps):
-            next_state = torch.einsum("nlc,nlcb->nlb", next_state[:, :-num_steps], predictor)
+            next_state = torch.einsum("nlc,ncb->nlb", next_state[:, :-num_steps], predictor)
             # predictions.append(next_state)
 
         return next_state, next_state_ground_true
